5b2bfa63ea13579e6a0ff2b8aa324b1c562c60d6
Rohit Jaganathan <rohit.jagan@gmail.com>
2022-02-24 16:44:51 -0800

    connect to mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 const process = require('process');
 require('dotenv').config({ path: './config.env' });
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
