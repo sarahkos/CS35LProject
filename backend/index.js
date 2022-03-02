80fe93e61cb668e2254ec87fff2c00a1d151afb1
Grant Gilchrist <gilchrist@ucla.edu>
2022-03-01 17:46:20 -0800

    Added handling login request

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const process = require('process');
+const User = require('./models/user.js')
+const Profile = require('./models/profile.js')
 require('dotenv').config({ path: './config.env' });
 const port = process.env.PORT || 3000;
 
@@ -15,5 +17,21 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
+
+app.post("/login", async (req, res) => {
+    try{
+    const { username, password } = req.body;
+
+    const user = await User.findOne({username});
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        return res.status(200).json(user);
+        }
+    
+    return res.status(400).send("Invalid Login");
+    }
+
+    finally{}
+    });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
