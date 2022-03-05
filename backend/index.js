372872b0bfe7836b021643575a629a56fe2ad986
Rohit Jaganathan <rohit.jagan@gmail.com>
2022-03-05 10:48:48 -0800

    added routes for retrieving current user and updating bio

--- backend/index.js
+++ backend/index.js
@@ -137,6 +137,52 @@ router.post("/users/logout", async (req, res, next) => {
 
 });
 
+router.get("/users/self", async (req, res, next) => {
+
+    try {            
+        if (req.isAuthenticated()) {
+            return res.status(200).json({
+                user: req.user,
+                msg: "User information retrieved.",
+            });
+        }
+        else {
+            return res.status(400).json({
+                err: "No user is currently logged in.",
+            });
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            err
+        })
+    }
+
+});
+
+router.post("/users/self/bio", async (req, res, next) => {
+
+    try {            
+        if (req.isAuthenticated()) {
+            await User.updateOne({ _id: req.user._id }, { bio: req.body.bio });
+            return res.status(201).json({
+                msg: "User bio updated successfully.",
+            });
+        }
+        else {
+            return res.status(400).json({
+                err: "No user is currently logged in.",
+            });
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            err
+        })
+    }
+
+});
+
 app.use('/api', router);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
