58adb3b477c2f7dae0ba33770a1e9e6ded5d6df3
Grant Gilchrist <gilchrist@ucla.edu>
2022-03-08 12:32:01 -0800

    Added image uploading for Recipies

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -19,6 +19,7 @@ const recipe = new mongoose.Schema({
     },
   }],
   date: { type: Date, default: Date.now },
+  image: { type: String, required: true }
 });
 
 recipe.virtual('likes').get(function() {
