b54549c7039f0a37798321c2239e125515ead49a
Rohit Jaganathan <rohit.jagan@gmail.com>
2022-03-08 10:36:35 -0800

    added routes for following users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -50,4 +50,82 @@ router.get("/:username", async (req, res, next) => {
 
 });
 
+router.get("/:username/followers", async (req, res, next) => {
+
+    try {            
+        const user = await User.findOne({ username: req.params.username }).populate("followers");
+        if (!user) {
+            return res.status(404).json({
+                err: "User not found.",
+            });
+        }
+        return res.status(200).json({
+            followers: user.followers,
+            msg: "Followers retrieved successfully.",
+        });
+
+    } catch (err) {
+        return res.status(400).json({
+            err
+        })
+    }
+
+});
+
+router.post("/:username/followers/follow", async (req, res, next) => {
+
+    try {            
+        const user = await User.findOneAndUpdate({ username: req.params.username }, {
+            $addToSet: { followers: req.user._id }
+        }, { new: true }).populate("followers");
+        if (!user) {
+            return res.status(404).json({
+                err: "User not found.",
+            });
+        }
+        const self = await User.findByIdAndUpdate(req.user._id, {
+            $addToSet: { following: user._id }
+        }, { new: true }).populate("following");
+        return res.status(201).json({
+            following: self.following,
+            followers: user.followers,
+            msg: "Followed successfully.",
+        });
+
+    } catch (err) {
+        return res.status(400).json({
+            err
+        })
+    }
+
+});
+
+router.post("/:username/followers/unfollow", async (req, res, next) => {
+
+    try {            
+        const user = await User.findOneAndUpdate({ username: req.params.username }, {
+            $pull: { followers: req.user._id }
+        }, { new: true }).populate("followers");
+        if (!user) {
+            return res.status(404).json({
+                err: "User not found.",
+            });
+        }
+        const self = await User.findByIdAndUpdate(req.user._id, {
+            $pull: { following: user._id }
+        }, { new: true }).populate("following");
+        return res.status(201).json({
+            following: self.following,
+            followers: user.followers,
+            msg: "Unfollowed successfully.",
+        });
+
+    } catch (err) {
+        return res.status(400).json({
+            err
+        })
+    }
+
+});
+
 module.exports = router;
