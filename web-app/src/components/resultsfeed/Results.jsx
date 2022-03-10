35849c27154bb4e92ed77be066494c65bdb10746
Hee Jun Lee <leeheejun@g.ucla.edu>
2022-03-09 21:59:15 -0800

    forgot to remove not needed imports

--- web-app/src/components/resultsfeed/Results.jsx
+++ web-app/src/components/resultsfeed/Results.jsx
@@ -1,11 +1,5 @@
-import Post from "../post/Post";
-import Share from "../share/Share";
 import "./feed.css";
 // import { Posts } from "../../dummyData";
-import { useEffect, useState } from "react";
-import axios from "axios"
-import { red } from "@mui/material/colors";
-
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export default function ResultsFeed() {
